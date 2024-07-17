0f6d06c166fabff489a77f654884c34c3bc5f811
Arturo Marzal <amarzalhiguero01@gmail.com>
2024-07-17 20:27:51 +0200

    Commit 7 - retoques 2

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,6 @@ from streamlit_folium import folium_static
 # Ruta al archivo CSV
 csv_file_path = 'C:/Users/amarz/PycharmProjects/Streamlit_Idealista/data/viviendas_PROD_v2.csv'
 MOD_file_path = 'C:/Users/amarz/PycharmProjects/Streamlit_Idealista/data/viviendas_MOD_V4.csv'
-tutecho_path = 'C:/Users/amarz/PycharmProjects/Streamlit_Idealista/data/tutecho.png'
-idealista_path = 'C:/Users/amarz/PycharmProjects/Streamlit_Idealista/data/idealista.png'
 
 # Cargar el archivo CSV en un DataFrame de pandas
 anuncios = pd.read_csv(csv_file_path, sep=",")
@@ -21,8 +19,9 @@ df = pd.read_csv(MOD_file_path, sep=",")
 for col in df.select_dtypes(include=[np.number]).columns:
     df[col].fillna(df[col].median(), inplace=True)
 
+# Imputar los valores nulos con 0 en lugar de la mediana
 for col in anuncios.select_dtypes(include=[np.number]).columns:
-    anuncios[col].fillna(anuncios[col].median(), inplace=True)
+    anuncios[col].fillna(0, inplace=True)
 
 # Separar la variable objetivo
 y = df['FINALPRICE_DISCOUNT']
@@ -54,65 +53,64 @@ def obtener_recomendacion_inversion(precio_original, precio_predicho):
 
 # Título y subtítulo
 st.title("TuTecho Search")
-st.subheader(
-    "Esta herramienta ha sido diseñada como producto personalizado de búsqueda de viviendas de potencial interés de adquisición para TuTecho"
+st.markdown(
+    '<p style="font-size:18px">Esta herramienta ha sido diseñada como producto personalizado de búsqueda de viviendas de potencial interés de adquisición para TuTecho</p>',
+    unsafe_allow_html=True
 )
 
-# Colocar las imágenes en la barra lateral
-st.sidebar.image(tutecho_path, width=60)
-st.sidebar.image(idealista_path, width=100)
+# Crear un panel de filtros en la barra lateral
+with st.sidebar:
+    st.header("Búsqueda de viviendas")
+    st.markdown("### Filtros")
 
-# Título del panel de filtros
-st.sidebar.title("Búsqueda de viviendas")
+    # Crear un recuadro para los filtros
+    with st.form("filter_form"):
+        # Filtro por número de habitaciones
+        num_habitaciones = st.selectbox(
+            "Número de Habitaciones", options=[1, 2, 3, 4, 5]
+        )
 
-# Crear un recuadro para los filtros
-with st.sidebar.form("filter_form"):
-    # Filtro por número de habitaciones
-    num_habitaciones = st.selectbox(
-        "Número de Habitaciones", options=[1, 2, 3, 4, 5]
-    )
+        # Filtro por precio de alquiler
+        precio_min, precio_max = st.slider(
+            "Precio de Alquiler", min_value=0, max_value=4000, value=(0, 4000), step=100
+        )
 
-    # Filtro por precio de alquiler
-    precio_min, precio_max = st.slider(
-        "Precio de Alquiler", min_value=0, max_value=4000, value=(0, 4000), step=100
-    )
+        # Filtro por rentabilidad requerida
+        rentabilidad_requerida = st.slider(
+            "Rentabilidad Requerida (%)", min_value=3, max_value=8, value=3, step=1
+        ) / 100
 
-    # Filtro por rentabilidad requerida
-    rentabilidad_requerida = st.slider(
-        "Rentabilidad Requerida (%)", min_value=3, max_value=8, value=3, step=1
-    ) / 100
+        # Lista de barrios (ordenada alfabéticamente)
+        barrios = sorted([
+            'Pau de Carabanchel', 'Palacio', 'Malasaña-Universidad', 'Sol', 'Lavapiés-Embajadores', 'Imperial',
+            'Huertas-Cortes', 'Acacias', 'Palomeras sureste', 'Portazgo', 'Numancia', 'Ventas', 'Recoletos',
+            'Goya', 'Guindalera', 'Prosperidad', 'Palos de Moguer', 'Jerónimos', 'Chueca-Justicia', 'Almagro',
+            'Trafalgar', 'Argüelles', 'Arapiles', 'Chopera', 'Delicias', 'Legazpi', 'Pacífico', 'Adelfas',
+            'Niño Jesús', 'Pueblo Nuevo', 'Quintana', 'Colina', 'Pinar del Rey', 'Bernabéu-Hispanoamérica',
+            'Nueva España', 'Castilla', 'Cuatro Caminos', 'Valdeacederas', 'Berruguete', 'Gaztambide', 'Estrella',
+            'Ibiza', 'Castellana', 'Lista', 'Fuente del Berro', 'El Viso', 'Bellas Vistas', 'Sanchinarro',
+            'Virgen del Cortijo - Manoteras', 'Butarque', 'Casco Histórico de Vallecas',
+            'Ensanche de Vallecas - La Gavia',
+            'Nuevos Ministerios-Ríos Rosas', 'Peñagrande', 'Valdezarza', 'La Paz', 'Tres Olivos - Valverde',
+            'Mirasierra',
+            'Las Tablas', 'Casa de Campo', 'Valdemarín', 'Ciudad Jardín', 'Cuzco-Castillejos', 'Costillares',
+            'Ventilla-Almenara', 'Vallehermoso', 'Ciudad Universitaria', 'Campo de las Naciones-Corralejos',
+            'Arroyo del Fresno', 'Águilas', 'San Isidro', 'Vista Alegre', 'Puerta Bonita', 'Buena Vista', 'Orcasitas',
+            'Entrevías', 'San Diego', 'Marroquina', 'El Pardo', 'El Plantío', 'Fuentelarreina', 'Pilar', 'Montecarmelo',
+            'Aravaca', 'Los Cármenes', 'Puerta del Ángel', 'Lucero', 'Aluche', 'Campamento', 'Comillas', 'Moscardó',
+            'Opañel', 'Abrantes', 'San Fermín', 'Zofío', 'Fontarrón', 'San Pascual', 'Canillas', 'San Andrés',
+            'Los Rosales', 'Cuatro Vientos', '12 de Octubre-Orcasur', 'Pradolongo', 'Almendrales', 'Palomeras Bajas',
+            'Pavones', 'Horcajo', 'Vinateros', 'Media Legua', 'Concepción', 'Simancas', 'San Juan Bautista', 'Atalaya',
+            'Palomas', 'Conde Orgaz-Piovera', 'Valdebebas - Valdefuentes', 'Apóstol Santiago', 'Timón', 'Arcos',
+            'Los Ángeles', 'San Cristóbal', 'Santa Eugenia', 'Casco Histórico de Vicálvaro',
+            'Valdebernardo - Valderribas',
+            'Ambroz', 'El Cañaveral - Los Berrocales', 'Salvador', 'Hellín', 'Amposta', 'Rosas', 'Canillejas', 'Rejas',
+            'Alameda de Osuna', 'Aeropuerto', 'Casco Histórico de Barajas'
+        ])
+        barrio = st.selectbox("Barrio", barrios)
 
-    # Lista de barrios (ordenada alfabéticamente)
-    barrios = sorted([
-        'Pau de Carabanchel', 'Palacio', 'Malasaña-Universidad', 'Sol', 'Lavapiés-Embajadores', 'Imperial',
-        'Huertas-Cortes', 'Acacias', 'Palomeras sureste', 'Portazgo', 'Numancia', 'Ventas', 'Recoletos',
-        'Goya', 'Guindalera', 'Prosperidad', 'Palos de Moguer', 'Jerónimos', 'Chueca-Justicia', 'Almagro',
-        'Trafalgar', 'Argüelles', 'Arapiles', 'Chopera', 'Delicias', 'Legazpi', 'Pacífico', 'Adelfas',
-        'Niño Jesús', 'Pueblo Nuevo', 'Quintana', 'Colina', 'Pinar del Rey', 'Bernabéu-Hispanoamérica',
-        'Nueva España', 'Castilla', 'Cuatro Caminos', 'Valdeacederas', 'Berruguete', 'Gaztambide', 'Estrella',
-        'Ibiza', 'Castellana', 'Lista', 'Fuente del Berro', 'El Viso', 'Bellas Vistas', 'Sanchinarro',
-        'Virgen del Cortijo - Manoteras', 'Butarque', 'Casco Histórico de Vallecas',
-        'Ensanche de Vallecas - La Gavia',
-        'Nuevos Ministerios-Ríos Rosas', 'Peñagrande', 'Valdezarza', 'La Paz', 'Tres Olivos - Valverde',
-        'Mirasierra',
-        'Las Tablas', 'Casa de Campo', 'Valdemarín', 'Ciudad Jardín', 'Cuzco-Castillejos', 'Costillares',
-        'Ventilla-Almenara', 'Vallehermoso', 'Ciudad Universitaria', 'Campo de las Naciones-Corralejos',
-        'Arroyo del Fresno', 'Águilas', 'San Isidro', 'Vista Alegre', 'Puerta Bonita', 'Buena Vista', 'Orcasitas',
-        'Entrevías', 'San Diego', 'Marroquina', 'El Pardo', 'El Plantío', 'Fuentelarreina', 'Pilar', 'Montecarmelo',
-        'Aravaca', 'Los Cármenes', 'Puerta del Ángel', 'Lucero', 'Aluche', 'Campamento', 'Comillas', 'Moscardó',
-        'Opañel', 'Abrantes', 'San Fermín', 'Zofío', 'Fontarrón', 'San Pascual', 'Canillas', 'San Andrés',
-        'Los Rosales', 'Cuatro Vientos', '12 de Octubre-Orcasur', 'Pradolongo', 'Almendrales', 'Palomeras Bajas',
-        'Pavones', 'Horcajo', 'Vinateros', 'Media Legua', 'Concepción', 'Simancas', 'San Juan Bautista', 'Atalaya',
-        'Palomas', 'Conde Orgaz-Piovera', 'Valdebebas - Valdefuentes', 'Apóstol Santiago', 'Timón', 'Arcos',
-        'Los Ángeles', 'San Cristóbal', 'Santa Eugenia', 'Casco Histórico de Vicálvaro',
-        'Valdebernardo - Valderribas',
-        'Ambroz', 'El Cañaveral - Los Berrocales', 'Salvador', 'Hellín', 'Amposta', 'Rosas', 'Canillejas', 'Rejas',
-        'Alameda de Osuna', 'Aeropuerto', 'Casco Histórico de Barajas'
-    ])
-    barrio = st.selectbox("Barrio", barrios)
-
-    # Botón para aplicar filtros
-    submit_button = st.form_submit_button(label='Aplicar filtros')
+        # Botón para aplicar filtros
+        submit_button = st.form_submit_button(label='Aplicar filtros')
 
 # Filtrar el dataset según los filtros seleccionados y almacenarlo en el estado de la sesión
 if submit_button:
@@ -131,9 +129,15 @@ if submit_button:
             (anuncios_filtrados['FINALPRICE_DISCOUNT'] <= finalprice_discount_max)
         ]
 
-        columna_barrio = f"LOCATIONNAME_{barrio.replace(' ', '_')}"
-        if columna_barrio in anuncios_filtrados.columns:
-            anuncios_filtrados = anuncios_filtrados[anuncios_filtrados[columna_barrio] == 1]
+        # Buscar la columna del barrio seleccionada
+        barrio_col = None
+        for col in anuncios_filtrados.columns:
+            if col.startswith("LOCATIONNAME_") and col.split("LOCATIONNAME_")[1].replace("_", " ") == barrio:
+                barrio_col = col
+                break
+
+        if barrio_col:
+            anuncios_filtrados = anuncios_filtrados[anuncios_filtrados[barrio_col] == 1]
 
         # Preparar el DataFrame para la predicción
         anuncios_prediccion = anuncios_filtrados.drop(columns=['lon', 'lat', 'ASSETID', 'FINALPRICE_DISCOUNT'])
@@ -148,43 +152,35 @@ if submit_button:
             axis=1
         )
 
-        # Filtrar las filas según la diferencia porcentual entre precio predicho y precio original
-        def diferencia_porcentual(row):
-            precio_original = row['FINALPRICE_DISCOUNT']
-            precio_predicho = row['PREDICCION_PRECIO']
-            return abs(precio_predicho - precio_original) / precio_original <= 0.20
+        # Filtrar según la diferencia porcentual
+        anuncios_filtrados = anuncios_filtrados[
+            (abs(anuncios_filtrados['PREDICCION_PRECIO'] - anuncios_filtrados['FINALPRICE_DISCOUNT']) /
+             anuncios_filtrados['FINALPRICE_DISCOUNT']) <= 0.2
+        ]
 
-        anuncios_filtrados = anuncios_filtrados[anuncios_filtrados.apply(diferencia_porcentual, axis=1)]
-
-        # Asignar colores según la recomendación de inversión
+        # Asignar color según la recomendación de inversión
         anuncios_filtrados['color'] = anuncios_filtrados['RECOMENDACION_INVERSION'].apply(
             lambda x: "#25be4c" if x == "SI" else "#be2528"
         )
 
-        # Calcular la diferencia entre precio actual y precio predicho
-        anuncios_filtrados['DIFERENCIA'] = anuncios_filtrados['PREDICCION_PRECIO'] - anuncios_filtrados['FINALPRICE_DISCOUNT']
-
-        # Ordenar el DataFrame
-        anuncios_filtrados = anuncios_filtrados.sort_values(
-            by=['RECOMENDACION_INVERSION', 'DIFERENCIA'],
-            ascending=[False, False]
-        )
-
-        # Almacenar los resultados en el estado de la sesión
+        # Guardar los resultados en el estado de la sesión
         st.session_state['anuncios_filtrados_con_prediccion'] = anuncios_filtrados
 
 # Recuperar los datos filtrados del estado de la sesión
 anuncios_filtrados_con_prediccion = st.session_state.get('anuncios_filtrados_con_prediccion')
 
-# Crear pestañas para el mapa y la tabla de viviendas filtradas
+# Agregar las pestañas para mostrar el mapa y la tabla filtrada
 tabs = ["Mapa de viviendas", "Viviendas filtradas"]
 selected_tab = st.radio("Seleccionar vista", tabs)
 
-# Mostrar el mapa de viviendas
 if selected_tab == "Mapa de viviendas":
     st.header("Mapa de viviendas filtradas")
+    # Mostrar el mapa con los puntos filtrados si existe anuncios_filtrados_con_prediccion
     if anuncios_filtrados_con_prediccion is not None and 'lat' in anuncios_filtrados_con_prediccion.columns and 'lon' in anuncios_filtrados_con_prediccion.columns:
+        # Crear un mapa folium
         mymap = folium.Map(location=[40.4165, -3.70256], zoom_start=12)
+
+        # Iterar sobre las filas del DataFrame y añadir marcadores al mapa
         for index, row in anuncios_filtrados_con_prediccion.iterrows():
             color = row['color']
             folium.CircleMarker(
@@ -194,15 +190,22 @@ if selected_tab == "Mapa de viviendas":
                 fill=True,
                 fill_color=color,
                 fill_opacity=0.7,
-                popup=
-                      f"Precio predicción: {row['PREDICCION_PRECIO']:.2f}€<br>"
-                      f"Precio actual: {row['FINALPRICE_DISCOUNT']:.2f}€"
+                popup=f"<b>Recomendación de inversión:</b> {row['RECOMENDACION_INVERSION']}<br>"
+                      f"<b>Precio predicción:</b> {row['PREDICCION_PRECIO']:.2f}€<br>"
+                      f"<b>Precio actual:</b> {row['FINALPRICE_DISCOUNT']:.2f}€"
             ).add_to(mymap)
+
+        # Mostrar el mapa en Streamlit
         folium_static(mymap)
 
-# Mostrar la tabla de viviendas filtradas
 elif selected_tab == "Viviendas filtradas":
     st.header("Viviendas filtradas")
+    # Mostrar las filas filtradas del dataset con una columna adicional "¿Inversión recomendable?"
     if anuncios_filtrados_con_prediccion is not None:
-        columns_to_display = ['RECOMENDACION_INVERSION', 'FINALPRICE_DISCOUNT', 'PREDICCION_PRECIO'] + [col for col in anuncios_filtrados_con_prediccion.columns if col not in ['RECOMENDACION_INVERSION', 'FINALPRICE_DISCOUNT', 'PREDICCION_PRECIO', 'DIFERENCIA'] and not col.startswith('LOCATIONNAME_')]
+        # Ordenar por RECOMENDACION_INVERSION y diferencia porcentual
+        anuncios_filtrados_con_prediccion['DIFERENCIA_PORCENTUAL'] = (anuncios_filtrados_con_prediccion['PREDICCION_PRECIO'] - anuncios_filtrados_con_prediccion['FINALPRICE_DISCOUNT']) / anuncios_filtrados_con_prediccion['FINALPRICE_DISCOUNT']
+        anuncios_filtrados_con_prediccion.sort_values(by=['RECOMENDACION_INVERSION', 'DIFERENCIA_PORCENTUAL'], ascending=[False, False], inplace=True)
+
+        # Definir orden de columnas para mostrar
+        columns_to_display = ['RECOMENDACION_INVERSION', 'FINALPRICE_DISCOUNT', 'PREDICCION_PRECIO'] + [col for col in anuncios_filtrados_con_prediccion.columns if col != 'RECOMENDACION_INVERSION' and col != 'FINALPRICE_DISCOUNT' and col != 'PREDICCION_PRECIO' and not col.startswith('LOCATIONNAME_')]
         st.dataframe(anuncios_filtrados_con_prediccion[columns_to_display].reset_index(drop=True))
